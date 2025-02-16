afe14a22f87553db6f23a128560b7e2e39a19ad4
Daisuke Mutaguchi <mutaguchi@roy.hi-ho.ne.jp>
2025-02-17 02:56:37 +0900

    Add support for OpenAI compatible API.

--- Functions/Convert-PGTString.ps1
+++ Functions/Convert-PGTString.ps1
@@ -4,10 +4,10 @@ function Convert-PGTString
 {
     <#
 .SYNOPSIS
-Converts input strings based on the specified rule or examples using the OpenAI API.
+Converts input strings based on the specified rule or examples using the OpenAI or compatible API.
 
 .DESCRIPTION
-The Convert-PGTString function uses OpenAI's GPT-3.5-turbo (default) or GPT-4 (when -Gpt4 switch is specified) to transform input strings based on the provided rule or examples. It can handle various transformation scenarios and provides powerful string conversion capabilities. Make sure to set your OpenAI API key in the OPENAI_API_KEY environment variable before using this function.
+The Convert-PGTString function uses OpenAI's gpt-4o-mini (default) or other models (when -Model parameter is specified) to transform input strings based on the provided rule or examples. It can handle various transformation scenarios and provides powerful string conversion capabilities. Make sure to set your OpenAI API key in the OPENAI_API_KEY environment variable before using this function. OPENAI_API_URI environment variable can be set to use a custom API endpoint.
 
 .NOTES
 The Convert-PGTString function only invokes the OpenAI API once, even when multiple values are passed as input via a pipeline or parameters. However, if you execute the function within a loop construct like foreach, the API will be called multiple times. It is generally recommended to avoid executing the function within a loop.
@@ -21,8 +21,8 @@ An array of examples provided as strings in the format "Before=After" or as hash
 .PARAMETER Rule
 A string specifying the rule for transformation. Either Rule or Example (or both) must be provided.
 
-.PARAMETER Gpt4
-When specified, the function uses the GPT-4 model instead of the default GPT-3.5-turbo for the transformation.
+.PARAMETER Model
+When specified, uses the specified model for the transformation. The default model is gpt-4o-mini.
 
 .PARAMETER IncludeRule
 When specified, includes the rule in the output object.
@@ -92,7 +92,7 @@ Input Output
 This example demonstrates the use of Convert-PGTString with a single example in the form of a hashtable. The input numbers 1, 2, and 3 are transformed based on the provided example, which indicates that the transformation rule is to double the given number. The output objects include both the original input numbers and their transformed versions.
 
 .EXAMPLE
-PS> $array = Convert-PGTString -InputString 7,9,15 -Example "2=Yes","3=Yes","4=No","5=Yes","6=No","11=Yes" -IncludeRule -Gpt4 -CultureInfo en-US
+PS> $array = Convert-PGTString -InputString 7,9,15 -Example "2=Yes","3=Yes","4=No","5=Yes","6=No","11=Yes" -IncludeRule -CultureInfo en-US
 PS> $array.Rule
 Determine prime number or not (Yes/No)
 PS> $array.StringPairs | Where-Object Output -eq Yes
@@ -101,19 +101,59 @@ Input Output
 ----- ------
 7     Yes
 
-This example demonstrates the use of Convert-PGTString with multiple examples in the form of a string array. The input numbers 7, 9, and 15 are transformed based on the provided examples, which indicate the transformation rule is to determine whether the given numbers are prime (Yes) or not (No). By using the -Gpt4 switch, the accuracy of the transformation is improved. Specifying the CultureInfo as en-US may result in better accuracy compared to ja-JP in some cases. The output objects, which include the transformation rule and the input-output pairs, can be assigned to variables or passed through the pipeline for further processing.
+This example demonstrates the use of Convert-PGTString with multiple examples in the form of a string array. The input numbers 7, 9, and 15 are transformed based on the provided examples, which indicate the transformation rule is to determine whether the given numbers are prime (Yes) or not (No). Specifying the CultureInfo as en-US may result in better accuracy compared to ja-JP in some cases. The output objects, which include the transformation rule and the input-output pairs, can be assigned to variables or passed through the pipeline for further processing.
 
 In this case, the pipeline is used to filter the output objects based on the 'Yes' value, effectively showing only prime numbers in the result.
 #>    
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true, ValueFromPipeline)][string[]]$InputString,
-        [PSObject[]]$Example, [string]$Rule, [switch]$Gpt4, [switch]$IncludeRule, [switch]$IncludeInput, [CultureInfo] $CultureInfo
+        [PSObject[]]$Example, [string]$Rule, [string]$Model = "gpt-4o-mini", [switch]$IncludeRule, [switch]$IncludeInput, [CultureInfo] $CultureInfo
     )
 
     begin
     {
         $apiKey = $env:OPENAI_API_KEY
+        $grammar = ""
+        if (-not $env:OPENAI_API_URI)
+        {
+            $uri = "https://api.openai.com/v1/chat/completions"
+        }
+        else
+        {
+            $uri = $env:OPENAI_API_URI
+
+            # Copied from https://github.com/ggml-org/llama.cpp/blob/master/grammars/json.gbnf
+            # Copyright (c) 2023-2024 The ggml authors
+            # Licensed under the MIT License
+            $grammar = @'
+root   ::= object
+value  ::= object | array | string | number | ("true" | "false" | "null") ws
+
+object ::=
+  "{" ws (
+            string ":" ws value
+    ("," ws string ":" ws value)*
+  )? "}" ws
+
+array  ::=
+  "[" ws (
+            value
+    ("," ws value)*
+  )? "]" ws
+
+string ::=
+  "\"" (
+    [^"\\\x7F\x00-\x1F] |
+    "\\" (["\\bfnrt] | "u" [0-9a-fA-F]{4}) # escapes
+  )* "\"" ws
+
+number ::= ("-"? ([0-9] | [1-9] [0-9]{0,15})) ("." [0-9]+)? ([eE] [-+]? [0-9] [1-9]{0,15})? ws
+
+# Optional space: by convention, applied in this grammar after literal chars when allowed
+ws ::= | " " | "\n" [ \t]{0,20}
+'@
+        }
 
         $msgTable = @{Examples = @(
                 @{
@@ -181,8 +221,6 @@ Get-ChildItem | Where-Object {{$_.Extension -in ".png",".gif",".jpg"}}|Measure-O
         [cultureinfo]$culture = if ($null -eq $CultureInfo) { Get-UICulture } else { $CultureInfo }
         Import-LocalizedData -BindingVariable msgTable -UICulture $culture -ErrorAction:SilentlyContinue
         $examples = $msgTable.Examples
-
-        $model = $gpt4 ? "gpt-4":"gpt-3.5-turbo"
         
         $ruleWithExample = -not [string]::IsNullOrEmpty($Rule) -and $null -ne $Example
         $ruleOnly = -not $ruleWithExample -and $null -eq $Example
@@ -284,7 +322,9 @@ From the example, you should deduce the rule of converting the full first name t
 
 Do not include words from the input in the output.
 
-Now, as you''ll be given various strings as input, continue to deduce the rules and apply the transformations accordingly.'
+Now, as you''ll be given various strings as input, continue to deduce the rules and apply the transformations accordingly.
+
+Never include non-JSON strings in the output, such as warning text, annotations, etc. Always output JSON.'
         }
         elseif ($ruleWithExample)
         {
@@ -298,7 +338,9 @@ Using the provided rule and referring to the examples, apply the rule to the inp
 
 Do not include words from the input in the output.
 
-Now, as you''ll be given various rules and strings as input, continue to refer to the examples and apply the given rules to perform the transformations accordingly.'
+Now, as you''ll be given various rules and strings as input, continue to refer to the examples and apply the given rules to perform the transformations accordingly.
+
+Never include non-JSON strings in the output, such as warning text, annotations, etc. Always output JSON.'
         }
         elseif ($ruleOnly)
         {
@@ -312,14 +354,17 @@ Using the provided rule, apply the rule to the input strings, and output the res
 
 Do not include words from the input in the output.
 
-Now, as you''ll be given various rules and strings as input, continue to apply the given rules to perform the transformations accordingly.'
+Now, as you''ll be given various rules and strings as input, continue to apply the given rules to perform the transformations accordingly.
+
+Never include non-JSON strings in the output, such as warning text, annotations, etc. Always output JSON.'
         }
 
-        $messages = @([ordered]@{
+        $system_message = @([ordered]@{
                 "role"    = "system"
                 "content" = $system
-            }) +
-        @(
+            })
+
+        $example_messages = @(
             foreach ($e in $examples)
             {
                 $user, $assistant = ConvertTo-JsonStringArray -example $e `
@@ -335,8 +380,9 @@ Now, as you''ll be given various rules and strings as input, continue to apply t
                     "content" = $assistant
                 }
             }
-        ) +
-        @(
+        )
+
+        $user_message = @(
             [ordered]@{
                 "role"    = "user"
                 "content" = ConvertTo-JsonStringArray -example $inputDic `
@@ -345,22 +391,30 @@ Now, as you''ll be given various rules and strings as input, continue to apply t
                     -ruleOnly:$ruleOnly | Select-Object -First 1
             }
         )
+
+        $messages = $system_message + $example_messages + $user_message
         
         $body = [ordered]@{
-            model    = $model
-            messages = $messages
+            model      = $model
+            messages   = $messages
+            max_tokens = [int]([math]::Round((ConvertTo-Json $example_messages).Length / $example_messages.Length) * 2)
+        }
+        
+        if ($grammar -ne "")
+        {
+            $body.Add("grammar", $grammar)
         }
 
-        #$body
-        $uri = "https://api.openai.com/v1/chat/completions"
+        $headers = [ordered]@{}
 
-        $headers = @{
-            "Authorization" = "Bearer $($ApiKey)"
-            "Content-Type"  = "application/json"
+        if (-not [string]::IsNullOrEmpty($apiKey))
+        {
+            $headers.Add("Authorization", "Bearer $($ApiKey)")
         }
+        $headers.Add("Content-Type", "application/json")
 
         $body = $body | ConvertTo-Json 
-        $Byte = [System.Text.Encoding]::UTF8.GetBytes($body)
+        $byte = [System.Text.Encoding]::UTF8.GetBytes($body)
         $response = Invoke-RestMethod -Method 'POST' -Uri $uri -Headers $headers -Body $byte
 
         $jsonString = $response.choices[0].message.content.Trim()
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ PowerGenerativeToolkit is a comprehensive PowerShell module that leverages LLM t
 
 ## Features
 
-- Utilizes OpenAI's API to access gpt-3.5-turbo (default) and gpt-4 language models
+- Utilizes OpenAI's API or compatible APIs to access models like gpt-4o-mini (default)
 - Generates, transforms, and completes various types of text, including PowerShell functions
 - Optimized for use in English or Japanese environments, but can also work with other language settings
 
@@ -20,7 +20,7 @@ PowerGenerativeToolkit is a comprehensive PowerShell module that leverages LLM t
 
 ## Configuration
 
-To use PowerGenerativeToolkit, you'll need to set the `OPENAI_API_KEY` environment variable with your OpenAI API key.
+To use PowerGenerativeToolkit, you'll need to set the `OPENAI_API_KEY` environment variable with your OpenAI API key. If you are using an OpenAI-compatible API, set the `OPENAI_API_URI` environment variable with the URL of the API.
 
 ## Usage
 
@@ -84,8 +84,7 @@ Functions in the PowerGenerativeToolkit module, such as Convert-PGTString, only 
 
 ## Disclaimer
 
-Please be aware that this module uses OpenAI's API, which may have associated usage fees. Use the module at your own risk, and always be mindful of potential costs.
-
+Please be aware that this module uses the OpenAI API or compatible APIs, which may incur usage fees. Use this module at your own risk.
 
 # PowerGenerativeToolkit
 
@@ -93,7 +92,7 @@ PowerGenerativeToolkitは、LLMを利用してPowerShell関数を含むあらゆ
 
 ## 機能
 
-- OpenAIのAPIを利用して、gpt-3.5-turbo（デフォルト）およびgpt-4を使用可能
+- OpenAIのAPIまたは互換APIを利用して、gpt-4o-mini（デフォルト）等のモデルを使用可能
 - PowerShell関数を含むさまざまなタイプのテキストを生成、変換、補完（現在は変換のみサポート）
 - 日本語も問題なく利用できます
 
@@ -107,18 +106,16 @@ PowerGenerativeToolkitは、LLMを利用してPowerShell関数を含むあらゆ
 
 ## 設定
 
-PowerGenerativeToolkitを使用するには、`OPENAI_API_KEY`環境変数にOpenAI APIキーを設定する必要があります。
+PowerGenerativeToolkitを使用するには、`OPENAI_API_KEY`環境変数にOpenAI APIキーを設定する必要があります。OpenAI互換APIを使用する場合は、`OPENAI_API_URI`環境変数にAPIのベースURLを設定します。
 
 ## 使用方法
 
 現在、`Convert-PGTString`コマンドのみが実装されています。使用例については英語版ドキュメント参照。
 他の例は`Get-Help Convert-PGTString -Detailed`で見ることができます。
 
-GPT-3.5/4には日本語も通りますが、英語でRuleやExampleを与えた方が精度が上がるので、適宜、`-CultureInfo en-US`パラメータを指定すると良いでしょう。
-
 ## 注意
 PowerGenerativeToolkitモジュールの関数、例えばConvert-PGTStringなどは、パイプラインやパラメータを通じて複数の値が入力された場合でも、OpenAI APIを1回だけ呼び出します。ただし、foreachなどのループ構文内で関数を実行すると、APIが複数回呼び出されてしまいます。通常は、ループ構文内で関数を実行しないよう、注意してください。
 
 ## 免責事項
 
-このモジュールはOpenAIのAPIを使用しており、利用料金が発生します。また、モジュールのご利用は自己責任でお願いします。
+このモジュールはOpenAI APIや互換APIを使用しており、利用料金が発生する場合があります。また、モジュールのご利用は自己責任でお願いします。
